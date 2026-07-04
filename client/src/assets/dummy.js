87a587a3b785696da44c3172d977106cece9f585
swaraj cn <swarajcn774@gmail.com>
2026-07-04 20:18:22 +0530

    dummydata

--- client/src/assets/dummy.js
+++ client/src/assets/dummy.js
@@ -7,6 +7,9 @@ export const students = [
     {
         id:1,
         student_id:'10AC20',
-        student_pass:'12345678'
+        student_pass:'12345678',
+        student_name:'Aryan',
+        student_class:'10A',
+        student_teacher:'Ramya PK',
     }
 ]
