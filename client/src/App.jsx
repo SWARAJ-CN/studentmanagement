b78641963b601beb7f7d6f32086bfb60013a543a
swaraj cn <swarajcn774@gmail.com>
2026-07-10 15:55:50 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import { Toaster } from 'react-hot-toast'
+import React from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import { Toaster } from 'react-hot-toast';
+import About from './pages/About';
+import Feature from './pages/Feature';
+import Notice from './components/Notice';
+import Contact from './pages/Contact';
+import StudentLogin from './pages/StudentLogin';
+import StudentDash from './pages/StudentDash';
+import TeacherLogin from './teacher/pages/TeacherLogin';
+
+import TeacherLayout from './teacher/components/TeacherLayout';
+import TeacherDashboard from './teacher/pages/TeacherDashboard';
 
 const App = () => {
 
@@ -16,16 +27,33 @@ const App = () => {
 
   return (
     <>
-    <Toaster
-    position='bottom-rigth'
-    />
-    <Navbar/>
-     <Routes>
-      <Route path='/' element={<Home/>}/>
-     </Routes>
-     <Footer/>
+      <Toaster position="bottom-right" />
+
+      {!hideLayout && <Navbar />}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/features" element={<Feature />} />
+        <Route path="/notice" element={<Notice />} />
+        <Route path="/contact" element={<Contact />} />
+
+        {/* Student */}
+        <Route path="/student-login" element={<StudentLogin />} />
+        <Route path="/student-dashboard" element={<StudentDash />} />
+
+         {/* teacher login page */}
+        <Route path="/teacher-login" element={<TeacherLogin />} />
+
+        {/* teacher */}
+        <Route path="/teacher" element={<TeacherLayout />}>
+          <Route path="dashboard" element={<TeacherDashboard />} />
+        </Route>
+      </Routes>
+
+      {!hideLayout && <Footer />}
     </>
   );
 };
 
-export default App;
+export default App
